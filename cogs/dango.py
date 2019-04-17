97a4cdd865c7a8b1efab18c948a277adc32eb724
arielbeje <arielbeje@protonmail.com>
2019-04-17 14:11:46 +0300

    Added documentation for commands

--- cogs/dango.py
+++ cogs/dango.py
@@ -16,6 +16,7 @@ class DangoCog(commands.cog):
 
     @commands.command(name="setemoji")
     async def set_emoji(self, ctx: commands.Context, emoji: discord.Emoji):
+        """Set a different emoji for the bot"""
         await sql.execute("UPDATE servers SET emoji=? WHERE serverid=?", pickle.dumps(emoji), str(ctx.message.guild.id))
         em = discord.Embed(title="Set emoji",
                            description=str(emoji),
@@ -24,6 +25,7 @@ class DangoCog(commands.cog):
 
     @commands.command(name="setinterval")
     async def set_interval(self, ctx: commands.Context, interval: int):
+        """Set a different interval for the bot"""
         await sql.exceute("UPDATE servers SET interval=? WHERE serverid=?", interval, str(ctx.message.guild.id))
         em = discord.Embed(title="Updated interval",
                            description=f"Updated interval is {interval}",
@@ -32,20 +34,22 @@ class DangoCog(commands.cog):
 
     @commands.command()
     async def leaderboard(self, ctx: commands.context):
+        """Show the leaderboard"""
         people = await sql.fetch("SELECT memberid, score FROM scoreboard WHERE serverid=? ORDER BY score", str(ctx.message.guild.id))
         interval = await sql.fetch("SELECT interval FROM servers WHERE serverid=?", str(ctx.message.guild.id))
         emoji = pickle.loads(await sql.fetch("SELECT emoji FROM servers WHERE serverid=?", str(ctx.message.guild.id)))
-        total_dangos = sum([int(person[1]) for person in people])
-        members = [(ctx.guild.get_member(int(person[0])), person[1]) for person in people[:20]]
+        total_dangos = sum([int(person[1]) for person in people]) if people is not None else 0
         message = f"{emoji} **Scoreboard** {emoji}"
         message += f"\n\nTotal dangos: **{total_dangos}**"
         message += f"\nEvery **{interval}** messages"
-        scores = [item[0] for item in groupby(people, itemgetter(1))]
-        scoreboard = {score: [] for score in scores}
-        for member in members:
-            scoreboard[member[1]].append(member[0])
-        for score in scores:
-            people_with_score = [person.display_name for person in scoreboard[score]]
-            people_with_score.sort()
-            message += f"**{score}** {', '.join(people_with_score)}\n"
+        if people is not None:
+            members = [(ctx.guild.get_member(int(person[0])), person[1]) for person in people[:20]]
+            scores = [item[0] for item in groupby(people, itemgetter(1))]
+            scoreboard = {score: [] for score in scores}
+            for member in members:
+                scoreboard[member[1]].append(member[0])
+            for score in scores:
+                people_with_score = [person.display_name for person in scoreboard[score]]
+                people_with_score.sort()
+                message += f"**{score}** {', '.join(people_with_score)}\n"
         ctx.send(message)
